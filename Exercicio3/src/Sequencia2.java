da07c5c581de71e0a4fda64ab0b10717a49d445d
Dominique Ganime <92270970+dominiqueganime@users.noreply.github.com>
2022-02-16 16:49:55 +0000

    Add files via upload
    Solicitação de usuários, no qual, se esse possuir atributo negativo, o algoritmo deve retornar a soma dos usuarios anteriormente informados. 
    
    >> INCOMPLETO <<

---
+++ Exercicio3/src/Sequencia2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Sequencia2 {
+
+	public static void main(String[] args) {
+		
+// Coleta de dados de utilizador: 
+		
+		Scanner digite = new Scanner(System.in);	
+		
+	do {	
+		System.out.print("Digite o utilizador: ");
+		int numeroUtilizador = digite.nextInt();
+		
+	} while (numeroUtilizador > 0);
+	
+	
+	}
+	}
+
+		
---
+++ Exercicio3/src/Sequencia3.java
@@ -0,0 +1,7 @@
+//
+//public class Sequencia3 {
+//	
+//	for(){
+//	}
+//
+//}
